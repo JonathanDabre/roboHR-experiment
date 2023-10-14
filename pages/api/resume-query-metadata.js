9b00030d15813f3f3412b70092226971a282d773
alroy <alroypereira05@gmail.com>
2023-10-14 23:21:03 +0530

    completed

--- pages/api/resume-query-metadata.js
+++ pages/api/resume-query-metadata.js
@@ -15,6 +15,41 @@ import { PromptTemplate } from "langchain/prompts";
 export default async function handler(req, res) {
   try {
     //    do stuff
+    const { prompt } = req.body;
+
+    const client = new PineconeClient();
+    await client.init({
+      apiKey: process.env.PINECONE_API_KEY,
+      environment: process.env.PINECONE_ENVIRONMENT,
+    });
+
+    const pineconeIndex = client.Index(process.env.PINECONE_INDEX);
+
+    const vectorStore = await PineconeStore.fromExistingIndex(
+      new OpenAIEmbeddings(),
+      { pineconeIndex }
+    );
+
+    // Create Vector DBQA CHain
+    const model = new OpenAI();
+    const chain = VectorDBQAChain.fromLLM(model, vectorStore, {
+      k: 1,
+      returnSourceDocuments: true,
+    });
+
+    // Prompt Template
+    const promptTemplate = new PromptTemplate({
+      template: `Assume you are a Human Resources Director. According to the resumes, answer this question: {question}`,
+      inputVariables: ["question"],
+    });
+
+    const formattedPrompt = await promptTemplate.format({
+      question: prompt,
+    });
+
+    const response = await chain.call({
+      query: formattedPrompt,
+    });
 
     return res.status(200).json({
       output: response.text,
