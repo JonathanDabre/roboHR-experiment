5ee4f5bbf05875c68588196ff63e8e364ba35f6a
Falcao Zane Vijay <112317470+falcaozane@users.noreply.github.com>
2023-10-16 00:52:46 +0530

    ui

--- app/components/ButtonContainer.jsx
+++ app/components/ButtonContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ButtonContainer = ({ children }) => {
-  return <div className="flex items-center text-center mx-auto my-4 px-auto justify-center">{children}</div>;
+  return <div className="flex items-center text-center mx-auto my-2 px-auto justify-center">{children}</div>;
 };
 
 export default ButtonContainer;
